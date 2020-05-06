c9c2bba14b4d258d65a854345ab982c25841e5fe
DiegoNascimento1 <diegoh4money@gmail.com>
2020-05-05 22:11:09 -0300

    Criação da estrutura do include

---
+++ application/views/include/portal/cabecalho.php
@@ -0,0 +1,2 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<?php $this->load->view('include/portal/header.php'); ?>
---
+++ application/views/include/portal/rodape.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+
+<?php $this->load->view('include/portal/footer.php'); ?>
+<script>
+ <?= isset($js) ? $js : '' ?>
+</script>
