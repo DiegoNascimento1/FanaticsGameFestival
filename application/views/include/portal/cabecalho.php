c3efd11cbd6aebefd3ae0bc2ce9a2ec3e376a608
DiegoNascimento1 <diegoh4money@gmail.com>
2020-05-14 20:50:01 -0300

    Correção banner inicial

--- application/views/include/portal/cabecalho.php
+++ application/views/include/portal/cabecalho.php
@@ -8,7 +8,7 @@
                 <div class="row align-items-center">
                     <div class="col-lg-12">
                         <nav class="navbar navbar-expand-lg navbar-light">
-                            <a class="navbar-brand" href="index.html"> <img src="img/logo.png" alt="logo"> </a>
+                            <a class="navbar-brand" href="index.html"> <img src="<?= base_url('assets/img/logo.png')?>" alt="logo"> </a>
                             <button class="navbar-toggler" type="button" data-toggle="collapse"
                                 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                                 aria-expanded="false" aria-label="Toggle navigation">
@@ -48,11 +48,11 @@
                                         </div>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="contact.html">Contact</a>
+                                        <a class="nav-link" href="contact.html">Contato</a>
                                     </li>
                                 </ul>
                             </div>
-                            <a href="#" class="btn_1 d-none d-sm-block">Install Now</a>
+                            <a href="#" class="btn_1 d-none d-sm-block">Entrar</a>
                         </nav>
                     </div>
                 </div>
